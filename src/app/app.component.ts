8ea28957b38d47b1b5c49cc5c67d8ec6590b0e23
Thesasan1 <sasanshirdel.mail@gmail.com>
2025-08-26 11:51:47 +0330

    feat: add user-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
