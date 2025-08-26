cd1d9d8b7fc2589ae357e963e3a7a51848073cc6
Thesasan1 <sasanshirdel.mail@gmail.com>
2025-08-26 12:03:48 +0330

    feat: add container component and complete tasks component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserInputComponent } from "./user-input/user-input.component";
+import { ContainerComponent } from "./container/container.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserInputComponent],
+  imports: [RouterOutlet, UserInputComponent, ContainerComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
